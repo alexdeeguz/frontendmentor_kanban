34de28267cfc6eee989de67d6146c2f52d786625
alex <alexdg316@gmail.com>
2023-01-17 20:03:31 -0800

    Setup project

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 800;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
